bd54d2cc4c53d203ed12f896d07ce65526fc4aef
Charlie Ciccia <charlie.ciccia@singlewire.com>
2017-12-12 01:09:24 -0600

    parameterize port

--- wait-for-mysql.sh
+++ wait-for-mysql.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-until nc -z -v -w30 $PHPBB_TEST_DBHOST 3306
+until nc -z -v -w30 $PHPBB_TEST_DBHOST $PHPBB_TEST_DBPORT
 do
   echo "Waiting for database connection..."
   # wait for 5 seconds before check again
